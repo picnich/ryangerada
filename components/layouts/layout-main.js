102eada835900f05cfa6b5ecc0d2ef4c36ac4c86
Nich Pereira <pereira.nich@gmail.com>
2020-11-06 18:16:20 -0500

    Added Google Analytics

--- components/layouts/layout-main.js
+++ components/layouts/layout-main.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Header from '../header/Header'
 import Footer from '../footer/footer'
 import FooterWork from '../footer/footer-work'
@@ -7,6 +8,23 @@ import { motion } from "framer-motion"
 const LayoutMain = ({ children, nextProject, workPage = true, hYear = false, hProjNum = false, hClient = false }) => {
     return (
         <>
+            <Head>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DWRW384YS"></script>
+                    <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-6DWRW384YS');
+                    </script>
+                    `,
+                }}
+                />
+            </Head>
             <Header workPage={workPage} hYear={hYear} hProjNum={hProjNum} hClient={hClient} />
             <motion.div 
                 initial={{ opacity: 0 }}
