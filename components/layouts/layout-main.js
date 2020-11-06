b68fccb6f8e05fa6a0a60ecefec51f6712df4019
Nich Pereira <pereira.nich@gmail.com>
2020-11-06 18:25:06 -0500

    gtm bug lol

--- components/layouts/layout-main.js
+++ components/layouts/layout-main.js
@@ -9,18 +9,15 @@ const LayoutMain = ({ children, nextProject, workPage = true, hYear = false, hPr
     return (
         <>
             <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DWRW384YS"></script>
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
-                    <!-- Global site tag (gtag.js) - Google Analytics -->
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DWRW384YS"></script>
-                    <script>
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
                     gtag('config', 'G-6DWRW384YS');
-                    </script>
                     `,
                 }}
                 />
